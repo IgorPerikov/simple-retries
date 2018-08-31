1c8b2bb59222f33e532080ffe871003ae89d8ff6
Igor Perikov <perikov.igor@gmail.com>
2018-08-31 21:04:07 +0300

    base project structure

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.2.61"
+}
+
+group = "com.github.igorperikov"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+    maven("https://dl.bintray.com/spekframework/spek-dev")
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+
+    testImplementation("org.spekframework.spek2", "spek-dsl-jvm", "2.0.0-alpha.1")
+        .exclude("org.jetbrains.kotlin")
+    testRuntimeOnly("org.spekframework.spek2", "spek-runner-junit5", "2.0.0-alpha.1")
+        .exclude("org.jetbrains.kotlin")
+        .exclude("org.junit.platform")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform {
+        includeEngines("spek2")
+    }
+}
